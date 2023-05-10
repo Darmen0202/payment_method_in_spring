b3e376ddeeac500b2644c7ee813dd32114f2ae0b
Darmen0202 <darmenkubantov02@gmail.com>
2023-05-10 23:32:24 +0600

    changed some files

---
+++ springhomework/src/main/java/com/javastart/springhomework/service/TransferService.java
@@ -0,0 +1,34 @@
+package com.javastart.springhomework.service;
+
+import com.javastart.springhomework.entity.Account;
+import com.javastart.springhomework.entity.Bill;
+import com.javastart.springhomework.exceptions.NotDefaultBillException;
+import com.javastart.springhomework.utils.AccountUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class TransferService {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public TransferService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public Object transfer(Long accountIdFrom, Long accountIdTo, BigDecimal amount) {
+        Account accountFrom = accountService.getById(accountIdFrom);
+        Account accountTo = accountService.getById(accountIdTo);
+        Bill billFrom = AccountUtils.findDefaultBill(accountFrom);
+        Bill billTo = AccountUtils.findDefaultBill(accountTo);
+        billFrom.setAmount(billFrom.getAmount().subtract(amount));
+        billTo.setAmount(billTo.getAmount().add(amount));
+        accountService.update(accountFrom);
+        accountService.update(accountTo);
+        return "Success";
+    }
+
+}
